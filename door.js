dc2e1923eebd4d310191581ec3043b74874bb7bb
ITFESTCOMPETITION <smkgrisabajaya@gmail.com>
2026-09-16 22:46:54 +0700

    at home

--- door.js
+++ door.js
@@ -1,18 +1,22 @@
-const btn=document.getElementById("btnDoor");
-const textStatus=document.getElementById("textDanger");
-let doorLocked=true;
+const btnDoor = document.getElementById("btnDoor");
+const textDanger = document.getElementById("textDanger");
+const accessText = document.getElementById("accessText");
+let doorLocked = true;
 
-btn.addEventListener('click',()=>{
-    doorLocked=!doorLocked;
-    if(doorLocked){
-        textStatus.textContent="Aman";
-        btn.textContent="🔓Buka Pintu";
-        btn.style.color="#264d12";
-        btn.style.background="#00FF00";
-    }else{
-        textStatus.textContent="Perhatian!";
-        btn.textContent="🔒Tutup Pintu";
-        btn.style.color="#ba1111"
-        btn.style.background="#b57272"
+btnDoor.addEventListener("click", () => {
+    doorLocked = !doorLocked;
+    if (doorLocked) {
+        textDanger.textContent = "Aman";
+         btnDoor.style.background="#adff2f";
+        btnDoor.style.color="#130d0df7";
+        accessText.textContent = "Tertutup";
+        btnDoor.textContent = "🔓 Buka Pintu";
+    } else {
+        textDanger.textContent = "Perhatian!";
+        accessText.textContent = "Terbuka";
+        btnDoor.style.background="#d10e0ee3";
+        btnDoor.style.color="#efdadaf7";
+        btnDoor.textContent = "🔒 Tutup Pintu";
     }
+
 });
